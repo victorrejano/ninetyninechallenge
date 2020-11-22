f0ab1cedef22765e0c9661cc64967ccc97d45e13
Victor Rejano <victorrejanoapps@gmail.com>
2020-11-22 15:24:52 +0100

    [ADD] Stock domain model

---
+++ NinetyNineChallenge/Domain/Entities/Stock.swift
@@ -0,0 +1,15 @@
+//
+//  Stock.swift
+//  NinetyNineChallenge
+//
+//  Created by Victor Rejano on 22/11/20.
+//
+
+import Foundation
+
+struct Stock {
+    let name: String
+    let hot: Int
+    let reutersInstrumentCode: String
+    let category: String
+}
